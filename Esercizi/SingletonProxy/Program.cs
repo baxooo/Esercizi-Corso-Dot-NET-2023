a401384aab9446e1241cd7423fc78a7faa4e5a7c
alessandro di pea <fly.97@hotmail.it>
2023-11-28 17:01:46 +0100

    Singleton exercise

---
+++ Esercizi/SingletonProxy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SingletonProxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy1 = Proxy.Instance;
+            Proxy proxy2 = Proxy.Instance;
+
+            if (proxy1.Ip1 == proxy2.Ip1)
+                Console.WriteLine("proxy1 and proxy2 have the same ips 1 " + proxy1.Ip1);
+            else
+                Console.WriteLine($"proxy1 and proxy2 do not have the same ips 1 proxy1: {proxy1.Ip1}, proxy1: {proxy2.Ip1}");
+            
+            if (proxy1.Ip2 == proxy2.Ip2)
+                Console.WriteLine("proxy1 and proxy2 have the same ips 2 " + proxy1.Ip2);
+            else
+                Console.WriteLine($"proxy1 and proxy2 do not have the same ips 2 proxy1: {proxy1.Ip2}, proxy1: {proxy2.Ip2}");
+
+            if (proxy1.Ip3 == proxy2.Ip3)
+                Console.WriteLine("proxy1 and proxy2 have the same ips 3 " + proxy1.Ip3);
+            else
+                Console.WriteLine($"proxy1 and proxy2 do not have the same ips 3 proxy1: {proxy1.Ip3}, proxy1: {proxy2.Ip3}");
+
+            if (proxy1.Ip4 == proxy2.Ip4)
+                Console.WriteLine("proxy1 and proxy2 have the same ips 4 " + proxy1.Ip4);
+            else
+                Console.WriteLine($"proxy1 and proxy2 do not have the same ips 4 proxy1: {proxy1.Ip4}, proxy1: {proxy2.Ip4}");
+
+            if (proxy1.Equals(proxy2))
+                Console.WriteLine("proxy1 and proxy2 are the same instance");
+        }
+
+
+
+    }
+}
---
+++ Esercizi/SingletonProxy/Proxy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonProxy
+{
+    public class Proxy
+    {
+        private static Proxy _instance;
+        private static readonly object lockObject = new object();
+
+        private static string _ip1;
+        private static string _ip2;
+        private static string _ip3;
+        private static string _ip4;
+
+        public string Ip1 { get { return _ip1; } }
+        public string Ip2 { get { return _ip2; } }
+        public string Ip3 {  get { return _ip3; } }
+        public string Ip4 { get { return _ip4; } }
+
+        public static Proxy Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Proxy();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+
+        private Proxy()
+        {
+            Random rnd = new Random();
+            _ip1 = $"{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}";
+            _ip2 = $"{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}";
+            _ip3 = $"{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}";
+            _ip4 = $"{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}.{rnd.Next(1, 255)}";
+        }
+
+    }
+}
