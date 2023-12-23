950c5a26097d88f158bdd9b7b0860ef0a1e49501
alessandro di pea <fly.97@hotmail.it>
2023-12-23 10:57:57 +0100

    Generic Repository

---
+++ Esercizi/SpotiAPI/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotiAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SpotiAPI.Interfaces
+{
+    public interface IRepository<T> 
+        where T : class, new()
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+        public Task<ActionResult<T>> AddNewAsync(T entity);
+        public Task<ActionResult<T>> UpdateAsync(int id,T entity);
+        public Task<ActionResult<T>> DeleteAsync(int id);
+    }
+}
